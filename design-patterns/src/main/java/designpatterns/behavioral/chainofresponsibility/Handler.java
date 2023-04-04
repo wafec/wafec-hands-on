7deadbca9b185ab840470f199d4828fa244bdb92
Wallace Cardoso <wallace.cardoso@ic.unicamp.br>
2023-04-04 13:31:38 -0300

    Comments to handler in Chain of Responsibility

--- design-patterns/src/main/java/designpatterns/behavioral/chainofresponsibility/Handler.java
+++ design-patterns/src/main/java/designpatterns/behavioral/chainofresponsibility/Handler.java
@@ -1,5 +1,15 @@
 package designpatterns.behavioral.chainofresponsibility;
 
+/*
+	Main idea: pass along a request to multiple handlers in a sequence
+	Pros:
+		1. Open/Closed Principle: new handlers do not modify existent handlers
+		2. Single Responsibility: a handler does what it is meant to do only
+		3. Decoupling: invocation is decoupled from processing
+		4. Reuse: the chain can be built at runtime
+	Cons:
+		1. Requests might end up unhandled
+ */
 public interface Handler {
 	void execute( Request request );
 }
