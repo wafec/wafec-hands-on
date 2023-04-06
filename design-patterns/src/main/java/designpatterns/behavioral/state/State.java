f63bf5f88009dde3d8016734f9fbe9d72e1d827c
Wallace Cardoso <wallace.cardoso@ic.unicamp.br>
2023-04-06 07:37:27 -0300

    add comments for visitor and state

--- design-patterns/src/main/java/designpatterns/behavioral/state/State.java
+++ design-patterns/src/main/java/designpatterns/behavioral/state/State.java
@@ -1,5 +1,14 @@
 package designpatterns.behavioral.state;
 
+/*
+	Main idea: lets an object changing its state when reacting to changes.
+	Pros:
+		1. Single Responsibility: each state does a particular task
+		2. Open/Closed Principle: a new state implementation won't require any change
+		3. Conditionals are eliminated when using state classes
+	Cons:
+		1. Overkill for a few states which rarely change
+ */
 public interface State {
 	void operationA();
 
